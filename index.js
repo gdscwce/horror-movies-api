08c474b145b2cbf836df6227520c274604846f91
eduardonot <eduardo_not@hotmail.com>
2022-10-01 20:50:55 -0300

    feat: filter movies by director name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-const data = require('data.json');
+const data = require('./data.json');
 app.use(express.json());
 
 app.get('/', (req,res)=>{
@@ -22,10 +22,21 @@ app.get('/random', (req,res)=>{
 
 //list
 
+app.get('/director/:name', (req, res) => {
+  if (!req.params.name) {
+    return res.status(400).json({ message: 'missing name' })
+  }
+  const director = req.params.name.toString()
+  const filterData = data.filter(movie => {
+    return movie.director.includes(director)
+  })
+  return res.status(200).json(filterData)
+})
 
-// app.listen(5000, ()=>{
-//     console.log("Server has stated");
-// });
+
+app.listen(5000, ()=>{
+    console.log("Server has stated");
+});
 
 module.exports = app;
 
