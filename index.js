3d1a6d884fa3a1eb980c47ffc6e3144e4ae81ad7
John Simms <johnsi@tfg.co.za>
2022-10-01 11:08:05 +0200

    FIx: code formatting, path to data was wrong, enable app.listen.

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const express = require('express');
-const app = express();
-const PORT = 5000;
+const express = require('express')
+const app = express()
+const PORT = 5000
 
-const data = require('data.json');
-app.use(express.json());
+const data = require('./data.json')
+app.use(express.json())
 
-app.get('/', (req,res)=>{
-    return res.json(
-        {
-            message: "Horror Movies API"
-        }
-    )
-});
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Horror Movies API',
+    totalMovies: data.length,
+  })
+})
 
 //any random
-app.get('/random', (req,res)=>{
-    const numberOfElements = data.length;
-    const randomNumber = parseInt(Math.random()*numberOfElements);
-    return res.json(data[randomNumber])
-});
+app.get('/random', (req, res) => {
+  const numberOfElements = data.length
+  const randomNumber = parseInt(Math.random() * numberOfElements)
+  return res.json(data[randomNumber])
+})
 
-//list
+app.listen(PORT, () => {
+  console.info('Server has stated', 'http://localhost:' + PORT)
+})
 
-
-// app.listen(5000, ()=>{
-//     console.log("Server has stated");
-// });
-
-module.exports = app;
-
+module.exports = app
