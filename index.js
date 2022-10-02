7fda6cee6412cad43aed19c89bd1087a924edc7a
eduardonot <eduardo_not@hotmail.com>
2022-10-01 23:26:11 -0300

    feat: filter movies by director name

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-const data = require('data.json');
+const data = require('./data.json');
 app.use(express.json());
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     return res.json(
         {
             message: "Horror Movies API"
@@ -14,18 +14,25 @@ app.get('/', (req,res)=>{
 });
 
 //any random
-app.get('/random', (req,res)=>{
+app.get('/random', (req, res) => {
     const numberOfElements = data.length;
-    const randomNumber = parseInt(Math.random()*numberOfElements);
+    const randomNumber = parseInt(Math.random() * numberOfElements);
     return res.json(data[randomNumber])
 });
 
-//list
+app.get('/director/:name', (req, res) => {
+    if (!req.params.name) {
+        return res.status(400).json({ message: 'missing name' });
+    }
+    const director = req.params.name.toString();
+    const filterData = data.filter(movie => {
+        return movie.director.includes(director);
+    })
+    return res.status(200).json(filterData);
+})
 
-
-// app.listen(5000, ()=>{
-//     console.log("Server has stated");
-// });
+app.listen(5000, () => {
+    console.log("Server has stated");
+});
 
 module.exports = app;
-
